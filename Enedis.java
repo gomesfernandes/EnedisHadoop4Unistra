dd1597c7814d9748fd7132bda6259b2d4160c92c
Caty Gomes Fernandes <caty.gomes-fernandes@etu.unistra.fr>
2019-01-30 17:21:15 +0100

    Feat:Map per consumption range

--- Enedis.java
+++ Enedis.java
@@ -12,12 +12,36 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Enedis {
 
-    public static String[] SECTORLABELS = {"Residentiel",
-                                            "Professionnel",
-                                            "Agriculture",
-                                            "Industrie",
-                                            "Tertiaire",
-                                            "Autres"};
+    public static String[] SECTORLABELS =
+            {"Residentiel",
+            "Professionnel",
+            "Agriculture",
+            "Industrie",
+            "Tertiaire",
+            "Autres"};
+
+    public static String[] SURFACELABELS =
+            {"Surface petite",
+            "Surface moyenne",
+            "Surface large"};
+
+    public static String[] RESIDENCYLABELS =
+            {"Résidence ancienne",
+            "Résidence âge moyenne",
+            "Résidence récente"};
+
+    public static String[] CONSOLABLES =
+            {"Conso très basse",
+            "Conso basse",
+            "Conso modérée",
+            "Conso élevée"};
+
+    public static final double EPSILON = 0.01;
+
+    public static Float global_min_conso = Float.MAX_VALUE;
+    public static Float global_max_conso = Float.MIN_VALUE;
+    public static Float global_min_heating = Float.MAX_VALUE;
+    public static Float global_max_heating = Float.MIN_VALUE;
 
     public static String getMaxSector(Float[] avgs) {
         int maxIndex = 0;
@@ -30,141 +54,164 @@ public class Enedis {
         return SECTORLABELS[maxIndex];
     }
 
-    public static Float[] extractSectorAvgs(String[] cols) {
-        Float avg_residence = Float.parseFloat(cols[12]);
+    public static Float extractLargeSurfacePercentage(String[] cols) {
+        return Float.parseFloat(cols[31]) + Float.parseFloat(cols[32]);
+    }
 
-        Float avg_pro = Float.parseFloat(cols[15]);
+    public static Float extractOldResidencePercentage(String[] cols) {
+        return Float.parseFloat(cols[33]) + Float.parseFloat(cols[34]);
+    }
 
-        Float nb_agriculture = Float.parseFloat(cols[16]);
-        Float total_agriculture = Float.parseFloat(cols[17]);
-        Float avg_agriculture = total_agriculture / nb_agriculture ;
+    public static String extractSurfaceWithMaxPercentages(String[] cols) {
+        Float small_surface_percentage = Float.parseFloat(cols[27]) + Float.parseFloat(cols[28]);
 
-        Float nb_industry = Float.parseFloat(cols[18]);
-        Float total_industry = Float.parseFloat(cols[19]);
-        Float avg_industry = total_industry / nb_industry ;
+        Float medium_surface_percentage = Float.parseFloat(cols[29]) + Float.parseFloat(cols[30]);
 
-        Float nb_tertiary = Float.parseFloat(cols[20]);
-        Float total_tertiary = Float.parseFloat(cols[21]);
-        Float avg_tertiary = total_tertiary / nb_tertiary ;
+        Float large_surface_percentage = Float.parseFloat(cols[31]) + Float.parseFloat(cols[32]);
 
-        Float nb_other = Float.parseFloat(cols[22]);
-        Float total_other = Float.parseFloat(cols[23]);
-        Float avg_other = total_other / nb_other ;
+        Float max = Math.max(small_surface_percentage, Math.max(medium_surface_percentage,large_surface_percentage));
 
-        Float[] avgs = {avg_residence, avg_pro, avg_agriculture, avg_industry, avg_tertiary, avg_other};
+        if (Math.abs(max - small_surface_percentage) < EPSILON) {
+            return SURFACELABELS[0];
+        } else if (Math.abs(max - medium_surface_percentage) < EPSILON) {
+            return SURFACELABELS[1];
+        } else {
+            return SURFACELABELS[2];
+        }
+    }
 
-        return avgs;
+    public static String extractResidenceWithMaxPercentages(String[] cols) {
+        Float old_residence_percentage = Float.parseFloat(cols[33]) + Float.parseFloat(cols[34]);
+
+        Float medium_residence_percentage = Float.parseFloat(cols[35]) + Float.parseFloat(cols[36]);
+
+        Float new_residence_percentage = Float.parseFloat(cols[37]) + Float.parseFloat(cols[38]);
+
+        Float max = Math.max(old_residence_percentage, Math.max(medium_residence_percentage,new_residence_percentage));
+
+        if (Math.abs(max - old_residence_percentage) < EPSILON) {
+            return RESIDENCYLABELS[0];
+        } else if (Math.abs(max - medium_residence_percentage) < EPSILON) {
+            return RESIDENCYLABELS[1];
+        } else {
+            return RESIDENCYLABELS[2];
+        }
     }
 
     public static class PerDepartmentMapper extends Mapper<LongWritable, Text, Text, Text>{
         /**
-         * Per line, calculate the average consumption per site for each of the 6 sectors.
-         * Return the name of the sector with the biggest consumption.
          */
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
             String[] cols = value.toString().split(";");
-            String year = cols[0];
-            String departement = cols[7];
+            String commune = cols[2];
 
-            Float[] avgs = extractSectorAvgs(cols);
+            if (Float.parseFloat(cols[10]) == 0.0)
+                return; //pas de sites ENEDIS
 
-            String max_sector = getMaxSector(avgs);
+            Float avg_residence = Float.parseFloat(cols[12]);
 
-            context.write(new Text(departement), new Text(max_sector));
+            String taux_chauffage = cols[40];
+
+            StringBuilder output_value = new StringBuilder();
+            output_value.append(avg_residence.toString());
+            output_value.append(":");
+            output_value.append(extractSurfaceWithMaxPercentages(cols));
+            output_value.append(":");
+            output_value.append(extractResidenceWithMaxPercentages(cols));
+            output_value.append(":");
+            output_value.append(taux_chauffage);
+
+            context.write(new Text(commune), new Text(output_value.toString()));
         }
     }
 
-    public static class PerSectorCountReducer extends Reducer<Text,Text,Text,Text> {
+    public static class Reduce2 extends Reducer<Text,Text,Text,Text> {
 
-        /**
-         * Per department and per sector, return the number of communes for which the sector consumed the most.
-         */
         public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
-            Map<String,Integer> count_per_sector = new HashMap<String,Integer>();
+            List<Float> list_consumption = new ArrayList<Float>();
+            String surface = new String();
+            String residence = new String();
+            String heating_string = new String();
 
-            for (int i=0; i < SECTORLABELS.length ; i++)
-                count_per_sector.put(SECTORLABELS[i],0);
+            for (Text v : values) {
+                String[] content = v.toString().split(":");
+                Float consumption = Float.parseFloat(content[0]);
+                list_consumption.add(consumption);
 
-            for (Text sector : values) {
-                Integer oldvalue = count_per_sector.get(sector.toString());
-                count_per_sector.put(sector.toString(),oldvalue + 1);
+                surface = content[1];
+                residence = content[2];
+                heating_string = content[3];
             }
 
-            StringBuilder output_value = new StringBuilder();
-            output_value.append("");
-            String sector = new String();
+            Float heating = Float.parseFloat(heating_string);
 
-            for (int i=0; i < SECTORLABELS.length ; i++) {
-                sector = SECTORLABELS[i];
-                output_value.append(sector);
-                output_value.append(":");
-                output_value.append(count_per_sector.get(sector));
-                if (i!=SECTORLABELS.length-1)
-                    output_value.append(",");
+            Float avg = new Float(0);
+            for(Float entry : list_consumption) {
+                avg += entry;
+            }
+            avg= avg/list_consumption.size();
+
+            if (avg > global_max_conso) {
+                global_max_conso = avg;
+            }
+            if (avg < global_min_conso) {
+                global_min_conso = avg;
+            }
+            if (heating > global_max_heating) {
+                global_max_conso = heating;
+            }
+            if (heating < global_min_heating) {
+                global_min_conso = heating;
             }
 
-            context.write(key, new Text(output_value.toString()));
+            StringBuilder output = new StringBuilder();
+            output.append(surface);
+            output.append(":");
+            output.append(residence);
+            output.append(":");
+            output.append(heating_string);
+
+            context.write(new Text(avg.toString()), new Text(output.toString()));
         }
     }
 
+    public static String trancheConsommation(Float conso) {
+        Float middle = (global_max_conso-global_min_conso)/2;
+        Float quarter = (middle-global_min_conso)/2;
+        Float three_quarter = (global_max_conso-quarter);
 
-    public static class ExtractMaxSectorMapper extends Mapper<LongWritable, Text, Text, Text>{
+        if (conso < quarter)
+            return CONSOLABLES[0];
+        else if (conso < middle)
+            return CONSOLABLES[1];
+        else if (conso < three_quarter)
+            return CONSOLABLES[2];
+        else
+            return CONSOLABLES[3];
+    }
+
+    public static class Mapper2 extends Mapper<LongWritable, Text, Text, Text>{
 
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
-            String department = value.toString().split("\t")[0];
-            String counts = value.toString().split("\t")[1];
-            String[] sector_counts = counts.split(",");
 
-            Integer max = 0;
-            String maxSector = "";
+            String tranche = trancheConsommation(Float.parseFloat(value.toString().split("\t")[0]));
 
-            for (String entry : sector_counts) {
-                String sector = entry.split(":")[0];
-                Integer count = Integer.parseInt(entry.split(":")[1]);
-                if (count > max) {
-                    max = count;
-                    maxSector = sector;
-                }
-            }
-
-            StringBuilder output_value = new StringBuilder();
-            output_value.append(department);
-            output_value.append(":");
-            output_value.append(max.toString());
-
-            context.write(new Text(maxSector), new Text(output_value.toString()));
+            context.write(new Text(tranche),new Text(value.toString().split("\t")[1] ));
         }
 
     }
 
-    public static class ParSectorDepartmentWithMaxCountReducer extends Reducer<Text,Text,Text,Text> {
+    public static class Reducer2 extends Reducer<Text,Text,Text,Text> {
 
         public void reduce(Text key, Iterable<Text> values,
                            Context context
         ) throws IOException, InterruptedException {
-            Integer max = 0;
-            String max_dpt = "";
 
-            for (Text val : values) {
-                String department = val.toString().split(":")[0];
-                Integer count = Integer.parseInt(val.toString().split(":")[1]);
-                if (count > max) {
-                    max = count;
-                    max_dpt = department;
-                }
-            }
-
-            StringBuilder output_value = new StringBuilder();
-            output_value.append(max_dpt);
-            output_value.append(" (");
-            output_value.append(max.toString());
-            output_value.append(" communes)");
-
-            context.write(key,new Text(output_value.toString()));
+            context.write(key,new Text("ok"));
         }
     }
 
@@ -182,7 +229,7 @@ public class Enedis {
         job1.setOutputKeyClass(Text.class);
         job1.setOutputValueClass(Text.class);
         job1.setMapperClass(PerDepartmentMapper.class);
-        job1.setReducerClass(PerSectorCountReducer.class);
+        job1.setReducerClass(Reduce2.class);
 
         job1.setInputFormatClass(TextInputFormat.class);
         job1.setOutputFormatClass(TextOutputFormat.class);
@@ -200,8 +247,8 @@ public class Enedis {
         job2.setJarByClass(Enedis.class);
         job2.setOutputKeyClass(Text.class);
         job2.setOutputValueClass(Text.class);
-        job2.setMapperClass(ExtractMaxSectorMapper.class);
-        job2.setReducerClass(ParSectorDepartmentWithMaxCountReducer.class);
+        job2.setMapperClass(Mapper2.class);
+        job2.setReducerClass(Reducer2.class);
 
         job2.setInputFormatClass(TextInputFormat.class);
         job2.setOutputFormatClass(TextOutputFormat.class);
